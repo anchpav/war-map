c3f8c792916de597e9b394575737a04adf272f9b
anchpav <94590608+anchpav@users.noreply.github.com>
2026-03-07 21:39:45 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ import express from "express"
 import cors from "cors"
 import fs from "fs"
 import path from "path"
+import { fileURLToPath } from "url"
+
+// recreate __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
 const PORT = 3000
